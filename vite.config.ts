c94e3eada7d87b18b25997f77e1e0fc0a8847b6b
yjshi <495054021@qq.com>
2025-04-11 23:10:27 +0800

    fix img path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  publicDir: 'public',
 })
